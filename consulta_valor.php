278ea25b50e65d9a5983a576d814b3df1f850eda
Marcelo <sebastian.tenis.sc@gmail.com>
2021-01-10 09:14:53 -0300

    agregando los else y elseif

--- consulta_valor.php
+++ consulta_valor.php
@@ -24,3 +24,63 @@ if(($tipo =="tipo") && ($ciudad == "Ciudad")){
             array_push($id, $value->Id);
         }else{}
     }
+    echo json_encode($id);
+    
+}else if(($tipo == "tipo") && ($ciudad != "Ciudad")){
+    $contenido = json_decode(file_get_contents('data-1.json'));
+    $id = [];
+    foreach($contenido as $value){
+        if($ciudad == $value->Ciudad){
+            list($valor, $ceros) = explode(',', $value->Precio);
+            $valor;
+            list($eliminado, $contenidoLimpio) = explode('$', $valor);
+            $contenidoLimpio;
+            $total = $contenidoLimpio.$ceros;
+            if(($total >= $min) &&($total <= $max)){
+                array_push($id, $value->Id);
+            }
+        }else{
+
+        }
+    }
+    echo json_encode($id);
+}else if(($tipo != "tipo") && ($ciudad == "Ciudad")){
+    $contenido = json_decode(file_get_contents('data-1.json'));
+    $id = [];
+    foreach($contenido as $value){
+        if($tipo == $value->Tipo){
+            list($valor, $ceros) = explode(',', $value->Precio);
+            $valor;
+            list($eliminado, $contenidoLimpio) = explode('$', $valor);
+            $contenidoLimpio;
+            $total = $contenidoLimpio.$ceros;
+            if(($total >= $min) &&($total <= $max)){
+                array_push($id, $value->Id);
+            }
+        }else{
+
+        }
+    }
+    echo json_encode($id);
+}
+else if(($tipo != "tipo") && ($ciudad != "Ciudad")){
+    $contenido = json_decode(file_get_contents('data-1.json'));
+    $id = [];
+    foreach($contenido as $value){
+        if(($tipo == $value->Tipo) && ($ciudad == $value->Ciudad)){
+            list($valor, $ceros) = explode(',', $value->Precio);
+            $valor;
+            list($eliminado, $contenidoLimpio) = explode('$', $valor);
+            $contenidoLimpio;
+            $total = $contenidoLimpio.$ceros;
+            if(($total >= $min) &&($total <= $max)){
+                array_push($id, $value->Id);
+            }
+        }else{
+
+        }
+    }
+    echo json_encode($id);
+}
+
+?> 
