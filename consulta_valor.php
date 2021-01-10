92bff1cb869c5272aa65c15b8a1e6552fae0c610
Marcelo <sebastian.tenis.sc@gmail.com>
2021-01-10 09:10:32 -0300

    dividiendo los valores

--- consulta_valor.php
+++ consulta_valor.php
@@ -5,3 +5,11 @@
 $ciudad = $_GET['ciudad'];
 $tipo = $_GET['tipo'];
 $precio = $_GET['precio'];
+//Dividiendo los valores
+list($min, $max) = explode(';', $precio);
+
+if(($tipo =="tipo") && ($ciudad == "Ciudad")){
+    //Traer la informacion del json
+    $contenido = json_decode(file_get_contents('data-1.json'));
+    $id = [];
+    foreach ($contenido as $value) {
